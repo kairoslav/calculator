4e463cf1af074b78bf41a6ca07e31e38ba049f13
s.kayranov <svt210@yandex.ru>
2025-11-20 13:57:43 +0500

    Fix contract

--- src/main/java/ru/itmo/calculator/controller/CalculatorController.java
+++ src/main/java/ru/itmo/calculator/controller/CalculatorController.java
@@ -1,15 +1,15 @@
 package ru.itmo.calculator.controller;
 
 import java.util.List;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.RestController;
-import ru.itmo.calculator.openapi.api.DefaultApi;
+import ru.itmo.calculator.openapi.api.CalculatorApi;
 import ru.itmo.calculator.openapi.model.ExecuteProgramRequest;
 import ru.itmo.calculator.openapi.model.ExecuteProgramResponse;
 import ru.itmo.calculator.openapi.model.PrintedValue;
 
 @RestController
-public class DefaultApiController implements DefaultApi {
+public class CalculatorController implements CalculatorApi {
 
     @Override
     public ExecuteProgramResponse executeProgram(ExecuteProgramRequest executeProgramRequest) {
